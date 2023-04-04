394f4614ceffb2c2d8805a6491e9d278198a63e5
dongyeopCha <ckehdduq95@gmail.com>
2023-04-04 18:37:57 +0900

    feat: SnakeGame 스코어,게임 오버 기능 추가

--- src/pages/SnakePage.js
+++ src/pages/SnakePage.js
@@ -16,8 +16,11 @@ export default class SnakePage extends CoreComponent {
 
         const ctx = canvas.getContext('2d');
 
+        const baseLocation = 100;
+
         const snakeLocation = []; //뱀 좌표정보
         let tail = 1;
+        let gameStatus = false;
 
         //뱀 시작 좌표
         let snakeX = 1;
@@ -30,8 +33,8 @@ export default class SnakePage extends CoreComponent {
         let moveY = 0;
 
         const gridSize = 20;
-        const gridCountWidth = canvas.width / gridSize;
-        const girdCountHeight = canvas.width / gridSize;
+        const gridCountWidth = (canvas.width - baseLocation * 2) / gridSize;
+        const girdCountHeight = (canvas.width - baseLocation * 2) / gridSize;
 
         function keyDownHandler(e) {
             switch (e.key) {
@@ -56,8 +59,17 @@ export default class SnakePage extends CoreComponent {
 
         document.addEventListener('keydown', keyDownHandler);
 
+        let life = 3;
+        let score = 0;
         function start() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.strokeStyle = 'white';
+            ctx.strokeRect(baseLocation, baseLocation, canvas.width - baseLocation * 2, canvas.height - baseLocation * 2);
+
+            ctx.font = 'bold 50px serif';
+            ctx.fillStyle = 'white';
+            ctx.fillText(`LIFE: ${life}`, 100, 90);
+            ctx.fillText(`SCORE: ${score}`, 700, 90);
             snakeX += moveX;
             snakeY += moveY;
             if (snakeX < 0) {
@@ -74,11 +86,22 @@ export default class SnakePage extends CoreComponent {
             }
 
             ctx.fillStyle = 'lime';
-
             for (let i = 0; i < snakeLocation.length; i++) {
-                ctx.fillRect(snakeLocation[i].x * gridSize, snakeLocation[i].y * gridSize, gridSize - 2, gridSize - 2);
-                if (snakeLocation[i].x === snakeX && snakeLocation[i].y === snakeY) {
+                ctx.fillRect(snakeLocation[i].x * gridSize + baseLocation, snakeLocation[i].y * gridSize + baseLocation, gridSize - 2, gridSize - 2);
+                if (snakeLocation.length != 1 && snakeLocation[i].x === snakeX && snakeLocation[i].y === snakeY) {
                     tail = i + 1;
+                    life -= 1;
+                    if (life == 0) {
+                        clearInterval(intervalId);
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                        ctx.font = 'bold 100px serif';
+                        ctx.fillStyle = 'red';
+                        ctx.fillText('GAME OVER', (canvas.width - ctx.measureText('GAME OVER').width) / 2, 500);
+                        ctx.fillStyle = 'white';
+                        ctx.font = 'bold 50px serif';
+                        ctx.fillText(`SCORE: ${score}`, (canvas.width - ctx.measureText(`SCORE: ${score}`).width) / 2, 600);
+                    }
                 }
             }
             snakeLocation.push({ x: snakeX, y: snakeY });
@@ -86,14 +109,17 @@ export default class SnakePage extends CoreComponent {
                 snakeLocation.shift();
             }
             if (appleX === snakeX && appleY === snakeY) {
+                score += 10;
                 tail++;
                 appleX = Math.floor(Math.random() * gridCountWidth);
                 appleY = Math.floor(Math.random() * girdCountHeight);
             }
             ctx.fillStyle = 'red';
-            ctx.fillRect(appleX * gridSize, appleY * gridSize, gridSize - 2, gridSize - 2);
+            ctx.fillRect(appleX * gridSize + baseLocation, appleY * gridSize + baseLocation, gridSize - 2, gridSize - 2);
         }
 
-        setInterval(start, 60);
+        const intervalId = setInterval(() => {
+            start();
+        }, 60);
     }
 }
