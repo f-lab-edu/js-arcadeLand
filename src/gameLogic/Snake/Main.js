054572bb7a1e2843a6c3ad476952f7ed09d8a01a
dongyeopCha <ckehdduq95@gmail.com>
2023-04-04 23:47:05 +0900

    refactor: page컴포넌트와 게임 로직 분리

---
+++ src/gameLogic/Snake/Main.js
@@ -0,0 +1,159 @@
+export default class Snake {
+    #canvas;
+    #ctx;
+    #padding;
+    #gridSize;
+    #rowGridCount;
+    #columnGridCount;
+    #headSnakeLocation;
+    #appleLocation;
+    #snakeLocationList;
+    #tail;
+    #score;
+    #move;
+    #snakeWidth;
+    #snakeHeight;
+    #animationId;
+
+    constructor(canvas) {
+        //canvas
+        this.#canvas = canvas;
+        this.#ctx = canvas.getContext('2d');
+        this.#padding = 100;
+        //grid
+        this.#gridSize = 20;
+        this.#rowGridCount = (this.#canvas.width - this.#padding * 2) / this.#gridSize;
+        this.#columnGridCount = (this.#canvas.height - this.#padding * 2) / this.#gridSize;
+        //default location
+        this.#headSnakeLocation = { x: 1, y: 1 };
+        this.#appleLocation = { x: 4, y: 4 };
+        this.#snakeLocationList = [];
+        //information
+        this.#tail = 1;
+        this.#score = 0;
+        this.#move = { x: 0, y: 0 };
+        //snakeSize
+        this.#snakeWidth = this.#gridSize - 2;
+        this.#snakeHeight = this.#gridSize - 2;
+        this.#beforeStart();
+    }
+
+    #beforeStart() {
+        document.addEventListener(
+            'keydown',
+            (e) => {
+                if (e.key === 'Enter') {
+                    this.#animationId = setInterval(() => this.#animate(), 60);
+                    this.#setEvent();
+                }
+            },
+            { once: true }
+        );
+    }
+
+    #animate() {
+        this.#renderReset();
+        this.#informationRender();
+        this.#moveSnake();
+        this.#resetOverFlowSnakeLocation();
+        this.#snakeRender();
+        this.#appleRender();
+    }
+
+    #renderReset() {
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+        this.#ctx.strokeStyle = 'white';
+        this.#ctx.strokeRect(this.#padding, this.#padding, this.#canvas.width - this.#padding * 2, this.#canvas.height - this.#padding * 2);
+    }
+    #informationRender() {
+        this.#ctx.font = 'bold 50px serif';
+        this.#ctx.fillStyle = 'white';
+        this.#ctx.fillText(`SCORE: ${this.#score}`, 100, 90);
+    }
+
+    #resetOverFlowSnakeLocation() {
+        if (this.#headSnakeLocation.x < 0) {
+            this.#headSnakeLocation.x = this.#rowGridCount - 1;
+        } else if (this.#headSnakeLocation.x > this.#rowGridCount) {
+            this.#headSnakeLocation.x = 0;
+        } else if (this.#headSnakeLocation.y < 0) {
+            this.#headSnakeLocation.y = this.#columnGridCount - 1;
+        } else if (this.#headSnakeLocation.y > this.#columnGridCount - 1) {
+            this.#headSnakeLocation.y = 0;
+        }
+    }
+
+    #moveSnake() {
+        this.#headSnakeLocation.x += this.#move.x;
+        this.#headSnakeLocation.y += this.#move.y;
+        if (this.#appleLocation.x === this.#headSnakeLocation.x && this.#appleLocation.y === this.#headSnakeLocation.y) {
+            this.#score += 10;
+            this.#tail += 1;
+            this.#appleLocation.x = Math.floor(Math.random() * this.#rowGridCount);
+            this.#appleLocation.y = Math.floor(Math.random() * this.#columnGridCount);
+        }
+    }
+    #snakeRender() {
+        this.#ctx.fillStyle = 'lime';
+        this.#snakeLocationList.forEach((_, index) => {
+            this.#ctx.fillRect(
+                this.#snakeLocationList[index].x * this.#gridSize + this.#padding,
+                this.#snakeLocationList[index].y * this.#gridSize + this.#padding,
+                this.#snakeWidth,
+                this.#snakeHeight
+            );
+            if (
+                this.#snakeLocationList.length != 1 &&
+                this.#snakeLocationList[index].x === this.#headSnakeLocation.x &&
+                this.#snakeLocationList[index].y === this.#headSnakeLocation.y
+            ) {
+                this.#gameEnd();
+            }
+        });
+
+        this.#snakeLocationList.push(Object.assign({}, this.#headSnakeLocation));
+        while (this.#snakeLocationList.length > this.#tail) {
+            this.#snakeLocationList.shift();
+        }
+    }
+
+    #appleRender() {
+        this.#ctx.fillStyle = 'red';
+        this.#ctx.fillRect(
+            this.#appleLocation.x * this.#gridSize + this.#padding,
+            this.#appleLocation.y * this.#gridSize + this.#padding,
+            this.#snakeWidth,
+            this.#snakeHeight
+        );
+    }
+
+    #gameEnd() {
+        clearInterval(this.#animationId);
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+        this.#ctx.font = 'bold 100px serif';
+        this.#ctx.fillStyle = 'red';
+        this.#ctx.fillText('GAME OVER', (this.#canvas.width - this.#ctx.measureText('GAME OVER').width) / 2, 500);
+        this.#ctx.fillStyle = 'white';
+        this.#ctx.font = 'bold 50px serif';
+        this.#ctx.fillText(`SCORE: ${this.#score}`, (this.#canvas.width - this.#ctx.measureText(`SCORE: ${this.#score}`).width) / 2, 600);
+    }
+
+    #setEvent() {
+        document.addEventListener('keydown', (e) => {
+            switch (e.key) {
+                case 'ArrowLeft':
+                    this.#move = { x: -1, y: 0 };
+                    break;
+                case 'ArrowUp':
+                    this.#move = { x: 0, y: -1 };
+                    break;
+                case 'ArrowRight':
+                    this.#move = { x: 1, y: 0 };
+                    break;
+                case 'ArrowDown':
+                    this.#move = { x: 0, y: 1 };
+                    break;
+            }
+        });
+    }
+}
--- src/pages/SnakePage.js
+++ src/pages/SnakePage.js
@@ -1,5 +1,6 @@
 import CoreComponent from '../core/CoreComponent';
 import RetroContainerComponent from '../components/RetroContainerComponent';
+import Snake from '../gameLogic/Snake/Main';
 export default class SnakePage extends CoreComponent {
     setHTML() {
         return `
@@ -12,114 +13,6 @@ export default class SnakePage extends CoreComponent {
     }
     setup() {
         //1080px 기준
-        const canvas = document.getElementById('retroCanvas');
-
-        const ctx = canvas.getContext('2d');
-
-        const baseLocation = 100;
-
-        const snakeLocation = []; //뱀 좌표정보
-        let tail = 1;
-        let gameStatus = false;
-
-        //뱀 시작 좌표
-        let snakeX = 1;
-        let snakeY = 1;
-        //먹이 시작 좌표
-        let appleX = 4;
-        let appleY = 4;
-        //좌표 이동
-        let moveX = 0;
-        let moveY = 0;
-
-        const gridSize = 20;
-        const gridCountWidth = (canvas.width - baseLocation * 2) / gridSize;
-        const girdCountHeight = (canvas.width - baseLocation * 2) / gridSize;
-
-        function keyDownHandler(e) {
-            switch (e.key) {
-                case 'ArrowLeft':
-                    moveX = -1;
-                    moveY = 0;
-                    break;
-                case 'ArrowUp':
-                    moveX = 0;
-                    moveY = -1;
-                    break;
-                case 'ArrowRight':
-                    moveX = 1;
-                    moveY = 0;
-                    break;
-                case 'ArrowDown':
-                    moveX = 0;
-                    moveY = 1;
-                    break;
-            }
-        }
-
-        document.addEventListener('keydown', keyDownHandler);
-
-        let life = 3;
-        let score = 0;
-        function start() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.strokeStyle = 'white';
-            ctx.strokeRect(baseLocation, baseLocation, canvas.width - baseLocation * 2, canvas.height - baseLocation * 2);
-
-            ctx.font = 'bold 50px serif';
-            ctx.fillStyle = 'white';
-            ctx.fillText(`LIFE: ${life}`, 100, 90);
-            ctx.fillText(`SCORE: ${score}`, 700, 90);
-            snakeX += moveX;
-            snakeY += moveY;
-            if (snakeX < 0) {
-                snakeX = gridCountWidth - 1;
-            }
-            if (snakeX > gridCountWidth) {
-                snakeX = 0;
-            }
-            if (snakeY < 0) {
-                snakeY = girdCountHeight - 1;
-            }
-            if (snakeY > girdCountHeight - 1) {
-                snakeY = 0;
-            }
-
-            ctx.fillStyle = 'lime';
-            for (let i = 0; i < snakeLocation.length; i++) {
-                ctx.fillRect(snakeLocation[i].x * gridSize + baseLocation, snakeLocation[i].y * gridSize + baseLocation, gridSize - 2, gridSize - 2);
-                if (snakeLocation.length != 1 && snakeLocation[i].x === snakeX && snakeLocation[i].y === snakeY) {
-                    tail = i + 1;
-                    life -= 1;
-                    if (life == 0) {
-                        clearInterval(intervalId);
-                        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                        ctx.font = 'bold 100px serif';
-                        ctx.fillStyle = 'red';
-                        ctx.fillText('GAME OVER', (canvas.width - ctx.measureText('GAME OVER').width) / 2, 500);
-                        ctx.fillStyle = 'white';
-                        ctx.font = 'bold 50px serif';
-                        ctx.fillText(`SCORE: ${score}`, (canvas.width - ctx.measureText(`SCORE: ${score}`).width) / 2, 600);
-                    }
-                }
-            }
-            snakeLocation.push({ x: snakeX, y: snakeY });
-            while (snakeLocation.length > tail) {
-                snakeLocation.shift();
-            }
-            if (appleX === snakeX && appleY === snakeY) {
-                score += 10;
-                tail++;
-                appleX = Math.floor(Math.random() * gridCountWidth);
-                appleY = Math.floor(Math.random() * girdCountHeight);
-            }
-            ctx.fillStyle = 'red';
-            ctx.fillRect(appleX * gridSize + baseLocation, appleY * gridSize + baseLocation, gridSize - 2, gridSize - 2);
-        }
-
-        const intervalId = setInterval(() => {
-            start();
-        }, 60);
+        new Snake(document.getElementById('retroCanvas'));
     }
 }
