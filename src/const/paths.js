bed2d4477dd375ea4e2f3a27aedc1d3e9ebb3c4a
dongyeopCha <ckehdduq95@gmail.com>
2023-04-06 00:31:53 +0900

    chore: Bird게임 경로 추가

--- src/const/paths.js
+++ src/const/paths.js
@@ -3,13 +3,14 @@ import BallPage from '../pages/BallPage';
 import SnakePage from '../pages/SnakePage';
 import GalagaPage from '../pages/GalagaPage';
 import MainPage from '../pages/MainPage';
-
+import BirdPage from '../pages/BirdPage.js';
 const paths = [
     { path: '', component: MainPage },
     { path: 'Tetris', component: TetrisPage },
     { path: 'Ball', component: BallPage },
     { path: 'Snake', component: SnakePage },
     { path: 'Galaga', component: GalagaPage },
+    { path: 'Bird', component: BirdPage },
 ];
 
 export default paths;
