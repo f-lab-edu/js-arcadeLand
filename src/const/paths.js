680a35d1013b7992e5e61f51d15ed1f1fb92631d
Dongyeop <64008899+dongyeopca@users.noreply.github.com>
2023-04-05 17:20:47 +0900

    chore: debugging용 로그제거
    Co-authored-by: f-lab-guell <117941186+f-lab-guell@users.noreply.github.com>

--- src/const/paths.js
+++ src/const/paths.js
@@ -5,8 +5,6 @@ import GalagaPage from '../pages/GalagaPage';
 import MainPage from '../pages/MainPage';
 
 const BASE_PATH = '/js-arcadeLand';
-console.log(process.env.NODE_ENV);
-console.log(BASE_PATH);
 const paths = [
     { path: `${BASE_PATH}/`, component: MainPage },
     { path: `${BASE_PATH}/Tetris`, component: TetrisPage },
